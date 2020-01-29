9a637826589ae0d2de7bb3b46b2a4432c53d80ac
yukikim <tki6ra@gmail.com>
2020-01-29 13:22:56 +0900

    superagentでAPI構築

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 //todo:POSTの為にbody-parserを追加
 // "npm install body-parser --save"
 const bodyParser = require('body-parser')
+//todo:非同期リクエストモジュール
+const request = require('superagent')
 
 //todo:静的アセット設定
 //publicディレクトリを静的ファイル格納ディレクトリに設定する
@@ -29,11 +31,56 @@ app.use(function(req, res, next) {
   next();
 })
 
+//todo:非同期でAPIデータ取得
+const callApi = (apiUrl, page = '1') => {
+    return new Promise((resolve, reject) => {
+        //todo:superagentの出番
+        request
+            .get(apiUrl)
+            .query({cd_ps: '1', bai: '0', YMD_biz: '', page: page})
+            .end((err, res) => {
+                if(err) {
+                    return reject(err)
+                }
+                resolve(res.body.Ki)
+            })
+    })
+}
+
+//todo:Promise呼出で取得する
+// callApi('http://www.pscube.jp/dedamajyoho-P-townDMMpachi/c739201/cgi-bin/nc-m03-001.php')
+//     .then((result) => {
+//         console.log('取得データは')
+//         console.dir(result)
+//     })
+//     .catch((error) => {
+//         console.log('エラー発生')
+//         console.dir(error)
+//     })
+
 //todo:ルーティング
 app.get('/', (req, res) => {
-    console.dir(req.query.name)
-    let param = req.query.name
-    res.send('Hello ' + param + ' さん!')
+    console.dir(req.query.page)
+    let param = req.query.page
+
+//todo:async/await呼出関数で取得する
+    getApi = async (pageNum) => {
+        const getData = await callApi('http://www.pscube.jp/dedamajyoho-P-townDMMpachi/c739201/cgi-bin/nc-m03-001.php', pageNum)
+        // console.log('async/awaitで取得')
+        // console.dir(getData)
+
+        //todo:取得した配列をmapで再配列を作成する
+        let getArr = []
+        getData.map((item, key) => {
+            getArr[key] = {
+                machine: item.nmk_kisyu
+            }
+        })
+        // console.dir(JSON.stringify(getArr))
+        //todo:JSONで返す
+        res.send(JSON.stringify(getArr))
+    }
+    getApi(param)
 })
 
 // app.use('/', require('./routes/index'))
